85c86a238e1a135ad611251703e07f772db961e6
croketephji <davidknapen@gmail.com>
2026-08-29 09:57:01 +0200

    Quartz sync: Aug 29, 2026, 9:57 AM

--- quartz.ts
+++ quartz.ts
@@ -1,14 +1,4 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
-import * as ExternalPlugin from "./.quartz/plugins"
-
-ExternalPlugin.Explorer({
-  sortFn: (a, b) => {
-    if (a.isFolder !== b.isFolder) {
-      return a.isFolder ? -1 : 1
-    }
-    return a.name.localeCompare(b.name, undefined, { numeric: true })
-  },
-})
 
 const config = await loadQuartzConfig()
 export default config
